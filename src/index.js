1a504e09a48e6c871234f64d7e9c62d055d305c4
Manoj <manoj@101digital.io>
2020-06-20 19:12:21 +0800

    redux saga integrated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@material-ui/styles";
+import { CssBaseline } from "@material-ui/core";
+
+import App from "app/App";
+import Themes from "themes/index";
+
+import configureStore from "./app/store/ConfigureStore";
+import * as serviceWorker from "./serviceWorker";
+
+let reactotroneStoreActions = null;
+
+if (process.env.NODE_ENV && process.env.NODE_ENV === "development") {
+  reactotroneStoreActions = require("./config/Reactotrone.config").default;
+}
+
+const store = configureStore(reactotroneStoreActions);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <ThemeProvider theme={Themes.default}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
